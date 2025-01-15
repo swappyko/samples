86cd70ee19f8548e266a3b7051100d31a69bd1c8
swapnilko <194364675+swapnilko@users.noreply.github.com>
2025-01-15 18:57:09 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -5,7 +5,7 @@ from email.mime.multipart import MIMEMultipart
 # Retrieve SMTP details from environment variables or GitHub secrets
 SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 465
-SMTP_USER = 'swapnilko@gmail.com'
+SMTP_USER = 'kondechipranjal@gmail.com'
 SMTP_PASSWORD = 'InfoSec@2022'
 TO_EMAIL = 'swapnil_konde@hotmail.com'
 SUBJECT = 'Test Email'
